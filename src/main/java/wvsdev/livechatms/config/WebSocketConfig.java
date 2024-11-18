4b4fec4b62f274f7ec13672970674feaa9e3c072
Wallace Vilela <wallace.vilela9@gmail.com>
2024-11-18 19:18:02 -0300

    Realizando a configuração do WebSocket

--- src/main/java/wvsdev/livechatms/config/WebSocketConfig.java
+++ src/main/java/wvsdev/livechatms/config/WebSocketConfig.java
@@ -2,8 +2,21 @@ package wvsdev.livechatms.config;
 
 import org.springframework.boot.autoconfigure.graphql.servlet.GraphQlWebMvcAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-public class WebSocketConfig extends GraphQlWebMvcAutoConfiguration.WebSocketConfiguration {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topics");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/live-chat-websocket");
+    }
 }
